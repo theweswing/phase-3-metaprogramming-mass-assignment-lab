2729d3319ec1673b9a880cccee0c95444371fe51
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-11-28 23:57:19 -0500

    commit tree cb8504de5ff95114109b2ba583f833a0ca26ed5c

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,8 @@
 class Person
-  # your code here
+  def initialize(hash)
+    hash.each_key do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", hash[key])
+    end
+  end
 end
